4b6bfc5ee99c1071fd018460c75d9e4a809ebbfe
Hiraku Okabe <okabee326@yahoo.co.jp>
2018-06-03 06:59:12 +0000

    send mail at blog creation

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(blog)
     @blog = blog
 
-    mail to: blog.email , subject: "ブログ作成の確認メール"
+    mail to: blog.user.email , subject: "ブログ作成の確認メール"
   end
 end
