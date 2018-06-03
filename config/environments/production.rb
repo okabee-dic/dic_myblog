df92a525eea39c91b9d5b92d54ea6eacc1fbf9ac
Hiraku Okabe <okabee326@yahoo.co.jp>
2018-06-03 06:27:01 +0000

    comit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  
   config.action_mailer.default_url_options = { host: 'infinite-tundra-14188.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
